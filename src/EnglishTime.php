7b012941e60edd405122a747c765f09b01ee8ea1
Bruno Paulin <brunopaulin@bpaulin.net>
2016-05-20 07:36:16 +0000

    cs

--- src/EnglishTime.php
+++ src/EnglishTime.php
@@ -3,24 +3,24 @@ namespace Bpaulin\EnglishTime;
 
 class EnglishTime {
   private function __construct() {}
-    public static function fromTimeStamp( $timestamp ){
-      $hour = date('H', $timestamp);
-      $minute = date('i', $timestamp);
-      $minute = round($minute/5)*5;
-      $nf = new \NumberFormatter("en", \NumberFormatter::SPELLOUT);
-      if ($minute>30) {
-        $hour++;
-        $minute = 60 - $minute;
-        $link = 'to';
-      }
-      else {
-        $link = 'past';
-      }
-      $hourDisplayed = $nf->format($hour);
-      if ($minute == 0) {
-        return "$hourDisplayed o'clock";
-      }
-      switch ($minute) {
+  public static function fromTimeStamp( $timestamp ){
+    $hour = date('H', $timestamp);
+    $minute = date('i', $timestamp);
+    $minute = round($minute/5)*5;
+    $nf = new \NumberFormatter("en", \NumberFormatter::SPELLOUT);
+    if ($minute>30) {
+      $hour++;
+      $minute = 60 - $minute;
+      $link = 'to';
+    }
+    else {
+      $link = 'past';
+    }
+    $hourDisplayed = $nf->format($hour);
+    if ($minute == 0) {
+      return "$hourDisplayed o'clock";
+    }
+    switch ($minute) {
       case 15:
         $minuteDisplayed = 'a quarter';
         break;
@@ -28,8 +28,8 @@ class EnglishTime {
         $minuteDisplayed = 'half';
         break;
       default:
-        $minuteDisplayed = $nf->format($minute);
+         $minuteDisplayed = $nf->format($minute);
       }
-      return "$minuteDisplayed $link $hourDisplayed";
-    }
+    return "$minuteDisplayed $link $hourDisplayed";
+  }
 }
